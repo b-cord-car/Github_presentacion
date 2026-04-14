09c65d4f99372a7fe8ea07369aeeba8861e4dfc5
Marisol <mari_sol24@unam.edu>
2026-04-14 00:40:34 -0600

    Pregunta 2 de la tarea 2

--- scripts/uno.R
+++ scripts/uno.R
@@ -19,5 +19,30 @@ tcrit <- qt(0.025,9)
 #p-value
 pval <- pt(tcalc,9,lower.tail = TRUE)*2
 #Se rechaza Ho
-t.test(antes,despues, paired = TRUE)
-#Hay diferencias en el peso de ratones antes y despues del tratamiento
+
+# Pregunta 2
+# Datos:
+n <- 10
+media_muestral <- 18
+varianza_muestral <- 9.5
+mu <- 15
+
+# Para calcular la desviación estándar
+sd <- sqrt(varianza_muestral)
+sd
+
+# Para calcular el error estándar (ee)
+ee <- sd/sqrt(n)
+ee
+
+# t calculada
+tcalc <- (media_muestral-mu)/ee
+tcalc
+
+# t crítica
+tcrit <- qt(0.95, df = 99, lower.tail = TRUE)
+tcrit
+
+# Cálculo de p-value
+p_value2 <- pt(9.733285, df= 99, lower.tail = FALSE)
+p_value2
